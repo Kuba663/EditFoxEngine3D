b5d429237f2ce5cf0abdd4c365802fc82557e89a
Kuba663 <kuba307@poczta.onet.pl>
2023-11-10 18:52:11 +0100

    Dodaj pliki projektów.

---
+++ EditFoxEngine3D/EditFoxEngine3D.cpp
@@ -0,0 +1,12 @@
+﻿// EditFoxEngine3D.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "EditFoxEngine3D.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ EditFoxEngine3D/EditFoxEngine3D.h
@@ -0,0 +1,8 @@
+﻿// EditFoxEngine3D.h: plik dołączany dla standardowych systemowych plików dołączanych,
+// lub pliki dołączane specyficzne dla projektu.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: W tym miejscu przywołaj dodatkowe nagłówki wymagane przez program.
