4bc9c97145f6fb527f21c924498dc2b8a67f556e
Kuba663 <kuba307@poczta.onet.pl>
2023-11-11 17:34:43 +0100

    feat: Ciąg graficzy ciąg dalszy

--- EditFoxEngine3D/EFEPipeline.cpp
+++ EditFoxEngine3D/EFEPipeline.cpp
@@ -6,11 +6,152 @@
 
 using EFE::render::CLASS;
 
-CLASS::Pipeline(const std::string& vertShaderFilename, const std::string& fragShaderFilename) {
-	this->createGraphicsPipeline(vertShaderFilename, fragShaderFilename);
+CLASS::Pipeline(Device& device, const std::string& vertShaderFilename, const std::string& fragShaderFilename, const PipelineConfigInfo& info)
+	: device{device} {
+	this->createGraphicsPipeline(vertShaderFilename, fragShaderFilename, info);
 }
 
-void CLASS::createGraphicsPipeline(const std::string& vertShaderFilename, const std::string& fragShaderFilename) {
+CLASS::~Pipeline() {
+	vkDestroyShaderModule(this->device.device(), this->vertShaderModule, nullptr);
+	vkDestroyShaderModule(this->device.device(), this->fragShaderModule, nullptr);
+	vkDestroyPipeline(this->device.device(), this->graphicsPipeline, nullptr);
+}
+
+void CLASS::createGraphicsPipeline(const std::string& vertShaderFilename, const std::string& fragShaderFilename, const PipelineConfigInfo& info) {
 	auto vertShader = readFileToVector(vertShaderFilename);
 	auto fragShader = readFileToVector(fragShaderFilename);
+
+	this->createShaderModule(vertShader, &vertShaderModule);
+	this->createShaderModule(fragShader, &fragShaderModule);
+
+	VkPipelineShaderStageCreateInfo shaderStages[2];
+	shaderStages[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	shaderStages[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
+	shaderStages[0].module = vertShaderModule;
+	shaderStages[0].pName = "main";
+	shaderStages[0].flags = 0;
+	shaderStages[0].pNext = nullptr;
+	shaderStages[0].pSpecializationInfo = nullptr;
+	shaderStages[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	shaderStages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+	shaderStages[1].module = fragShaderModule;
+	shaderStages[1].pName = "main";
+	shaderStages[1].flags = 0;
+	shaderStages[1].pNext = nullptr;
+	shaderStages[1].pSpecializationInfo = nullptr;
+
+	VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+	vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+	vertexInputInfo.vertexAttributeDescriptionCount = 0;
+	vertexInputInfo.vertexBindingDescriptionCount = 0;
+	vertexInputInfo.pVertexAttributeDescriptions = nullptr;
+	vertexInputInfo.pVertexBindingDescriptions = nullptr;
+
+	VkGraphicsPipelineCreateInfo pipelineInfo{};
+	pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+	pipelineInfo.stageCount = 2;
+	pipelineInfo.pStages = shaderStages;
+	pipelineInfo.pVertexInputState = &vertexInputInfo;
+	pipelineInfo.pInputAssemblyState = &info.inputAssemblyInfo;
+	pipelineInfo.pViewportState = &info.viewportInfo;
+	pipelineInfo.pRasterizationState = &info.rasterizationInfo;
+	pipelineInfo.pColorBlendState = &info.colorBlendInfo;
+	pipelineInfo.pDepthStencilState = &info.depthStencilInfo;
+	pipelineInfo.pDynamicState = nullptr;
+
+	pipelineInfo.layout = info.pipelineLayout;
+	pipelineInfo.renderPass = info.renderPass;
+	pipelineInfo.subpass = info.subpass;
+
+	pipelineInfo.basePipelineIndex = -1;
+	pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+
+	if (vkCreateGraphicsPipelines(this->device.device(), VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &graphicsPipeline) != VK_SUCCESS) 
+		throw new std::runtime_error("Nie mo¿na utworzyæ ci¹gu graficznego");
+}
+
+void CLASS::createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule) {
+	VkShaderModuleCreateInfo info{};
+	info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+	info.codeSize = code.size();
+	info.pCode = reinterpret_cast<const uint32_t*>(code.data());
+	if (vkCreateShaderModule(device.device(), &info, nullptr, shaderModule) != VK_SUCCESS) throw new std::runtime_error("Nie uda³o sie utworzyæ modu³u programu cieniuj¹cego");
+}
+
+EFE::render::PipelineConfigInfo CLASS::defaultPipelineConfigInfo(uint32_t width, uint32_t height) {
+	PipelineConfigInfo info{};
+
+	info.inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+	info.inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+	info.inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
+
+	info.viewport.x = 0.0f;
+	info.viewport.y = 0.0f;
+	info.viewport.width = static_cast<float>(width);
+	info.viewport.height = static_cast<float>(height);
+	info.viewport.minDepth = 0.0f;
+	info.viewport.maxDepth = 1.0f;
+
+	info.scissor.offset = { 0, 0 };
+	info.scissor.extent = { width, height };
+
+	info.viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+	info.viewportInfo.viewportCount = 1;
+	info.viewportInfo.pViewports = &info.viewport;
+	info.viewportInfo.scissorCount = 1;
+	info.viewportInfo.pScissors = &info.scissor;
+
+	info.rasterizationInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+	info.rasterizationInfo.depthClampEnable = VK_FALSE;
+	info.rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
+	info.rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
+	info.rasterizationInfo.lineWidth = 1.0f;
+	info.rasterizationInfo.cullMode = VK_CULL_MODE_NONE;
+	info.rasterizationInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+	info.rasterizationInfo.depthBiasEnable = VK_FALSE;
+	info.rasterizationInfo.depthBiasConstantFactor = 0.0f;  // Optional
+	info.rasterizationInfo.depthBiasClamp = 0.0f;           // Optional
+	info.rasterizationInfo.depthBiasSlopeFactor = 0.0f;     // Optional
+
+	info.multisampleInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+	info.multisampleInfo.sampleShadingEnable = VK_FALSE;
+	info.multisampleInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+	info.multisampleInfo.minSampleShading = 1.0f;           // Optional
+	info.multisampleInfo.pSampleMask = nullptr;             // Optional
+	info.multisampleInfo.alphaToCoverageEnable = VK_FALSE;  // Optional
+	info.multisampleInfo.alphaToOneEnable = VK_FALSE;       // Optional
+
+	info.colorBlendAttachment.colorWriteMask =
+		VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT |
+		VK_COLOR_COMPONENT_A_BIT;
+	info.colorBlendAttachment.blendEnable = VK_FALSE;
+	info.colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;   // Optional
+	info.colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;  // Optional
+	info.colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;              // Optional
+	info.colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;   // Optional
+	info.colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;  // Optional
+	info.colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;              // Optional
+
+	info.colorBlendInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+	info.colorBlendInfo.logicOpEnable = VK_FALSE;
+	info.colorBlendInfo.logicOp = VK_LOGIC_OP_COPY;  // Optional
+	info.colorBlendInfo.attachmentCount = 1;
+	info.colorBlendInfo.pAttachments = &info.colorBlendAttachment;
+	info.colorBlendInfo.blendConstants[0] = 0.0f;  // Optional
+	info.colorBlendInfo.blendConstants[1] = 0.0f;  // Optional
+	info.colorBlendInfo.blendConstants[2] = 0.0f;  // Optional
+	info.colorBlendInfo.blendConstants[3] = 0.0f;  // Optional
+
+	info.depthStencilInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+	info.depthStencilInfo.depthTestEnable = VK_TRUE;
+	info.depthStencilInfo.depthWriteEnable = VK_TRUE;
+	info.depthStencilInfo.depthCompareOp = VK_COMPARE_OP_LESS;
+	info.depthStencilInfo.depthBoundsTestEnable = VK_FALSE;
+	info.depthStencilInfo.minDepthBounds = 0.0f;  // Optional
+	info.depthStencilInfo.maxDepthBounds = 1.0f;  // Optional
+	info.depthStencilInfo.stencilTestEnable = VK_FALSE;
+	info.depthStencilInfo.front = {};  // Optional
+	info.depthStencilInfo.back = {};   // Optional
+
+	return info;
 }
--- EditFoxEngine3D/EFEPipeline.hpp
+++ EditFoxEngine3D/EFEPipeline.hpp
@@ -3,14 +3,44 @@
 
 #include "macros.h"
 #include <string>
+#include "EFEDevice.hpp"
 
 NS(EFE) {
 	NS(render) {
+		struct PipelineConfigInfo {
+			VkViewport viewport;
+			VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+			VkRect2D scissor;
+			VkPipelineViewportStateCreateInfo viewportInfo;
+			VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+			VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+			VkPipelineMultisampleStateCreateInfo multisampleInfo;
+			VkPipelineColorBlendAttachmentState colorBlendAttachment;
+			VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+			VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+			VkPipelineLayout pipelineLayout = nullptr;
+			VkRenderPass renderPass = nullptr;
+			uint32_t subpass = 0;
+		};
 		class Pipeline {
 		public:
-			Pipeline(const std::string& vertShaderFilename, const std::string& fragShaderFilename);
+			Pipeline(Device& device, const std::string& vertShaderFilename, const std::string& fragShaderFilename, const PipelineConfigInfo& info);
+			~Pipeline();
+
+			Pipeline(const Pipeline&) = delete;
+			void operator=(const Pipeline&) = delete;
+
+			static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
+
 		private:
-			void createGraphicsPipeline(const std::string& vertShaderFilename, const std::string& fragShaderFilename);
+			void createGraphicsPipeline(const std::string& vertShaderFilename, const std::string& fragShaderFilename, const PipelineConfigInfo& info);
+
+			void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
+
+			Device& device; // A Pipeline can't exist without a device!
+			VkPipeline graphicsPipeline;
+			VkShaderModule vertShaderModule;
+			VkShaderModule fragShaderModule;
 		};
 	}
 }
